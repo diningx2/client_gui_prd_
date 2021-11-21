976f065d0bf096e4f0ea3b4a073e3ea39c317cb1
diningX <hirosuke1309@gmail.com>
2021-11-21 16:23:25 +0900

    commit

--- time_series.py
+++ time_series.py
@@ -183,9 +183,10 @@ def time_series(df1):
     start_ = df1['date'].min()
     end_ = df1['date'].max()
 
+    date_range = np.sort(list(set([dt.strptime(str(i.year)+'-'+str(i.month)+'-'+'01', '%Y-%m-%d') for i in list(daterange(start_, end_))])))
     if len(date_range) == 0:
         date_range = [dt.strptime(str(start_.year)+'-'+str(start_.month)+'-'+'01', '%Y-%m-%d')]
-    st.write(date_range)
+ 
     
     date_option = [str(i)[:7].replace('-','/') for i in date_range]
     date_option = [i+'/01' for i in date_option]
